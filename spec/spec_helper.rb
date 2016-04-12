bf665fc59ab540bd9f80558dd938ad73752a1f6e
Kris Birov <kris.birov@gmail.com>
2016-04-12 17:39:51 +0300

    ccc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ require 'codeclimate-test-reporter'
 require 'simplecov'
 
 # save to CircleCI's artifacts directory if we're on CircleCI
-# if ENV['CIRCLE_ARTIFACTS']
-#   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
-#   SimpleCov.coverage_dir(dir)
-# end
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
 
 SimpleCov.start do
   formatter SimpleCov::Formatter::MultiFormatter.new([
